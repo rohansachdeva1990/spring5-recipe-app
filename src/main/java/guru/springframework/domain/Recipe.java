b8dedc718af3855a067f693dfa3a1d38afaa4a84
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-22 22:47:08 +0200

    Many To Many Relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -32,6 +32,14 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    // This is the owning entity for this relationship
+    // No cascade required because categories can exist without recipes
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Recipe() {
     }
 
@@ -129,5 +137,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
