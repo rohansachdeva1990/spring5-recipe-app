b9d9d863cd61af0aa937e4b7b3542e75f79d32a3
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-07-22 23:06:10 +0200

    Initialized database with data.sql

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -8,7 +8,6 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("ahsjkhfdsakj ajad gfjsa glkdfjgsakfgs kjgkgkg");
         return "index";
     }
 }
